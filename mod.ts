dd9dad667578d01c78c7745469f7dce2f074ec1a
Gnlow <gnlow@protonmail.com>
2023-12-08 14:50:47 +0900

    mm

--- mod.ts
+++ mod.ts
@@ -1,3 +1,26 @@
-import { tgz } from "https://deno.land/x/compress@v0.4.4/mod.ts"
+import { toFileUrl } from "https://deno.land/std@0.208.0/path/mod.ts"
 
-await tgz.uncompress("./test/1.ent", "./test/result")
+import { Untar } from "https://deno.land/std@0.208.0/archive/untar.ts"
+
+import {
+    readerFromStreamReader,
+    readableStreamFromReader,
+} from "https://deno.land/std@0.208.0/streams/mod.ts"
+
+const readableStream = await fetch(
+    toFileUrl(Deno.cwd()).href + "/test/1.ent"
+).then(res => res.body)!
+
+const decompressedReadableStream = readableStream!.pipeThrough(
+    new DecompressionStream("gzip")
+)
+const streamReader = decompressedReadableStream.getReader()
+
+const untar = new Untar(readerFromStreamReader(streamReader))
+
+for await (const entry of untar) {
+    console.log(entry.fileName)
+    if (entry.fileName.endsWith("/project.json")) {
+        await Deno.writeFile("test/project.json", readableStreamFromReader(entry))
+    }
+}
